5bd01fe2dc88c07dbbb92eb6b47b00b7d879f63b
Amanjeet kumar <146502426+Amanjeet-007@users.noreply.github.com>
2024-03-08 15:59:54 +0530

    Add files via upload
    added files

---
+++ sci-fi/script2.js
@@ -0,0 +1,23 @@
+gsap.from(".inc",{
+    duration:4,
+    delay:0.2,
+    width:"0%",                                             
+});
+gsap.from(".next i",{
+    duration:1,
+    x: 5,
+    delay:1,
+    repeat:10,
+});
+gsap.from(".team",{
+    duration:1,
+
+});
+gsap.from(".c",{
+    duration:150,
+    rotate:3600,
+    repeat:10,
+})
+let menu = document.querySelectorAll(".childone,childtwo,childthree,childfour");
+// menu =  (menu)
+// alert()
